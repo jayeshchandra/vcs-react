5dbd416d2ffa0196afa9bbd0be74b0939e72a7db
jayeshchandra <jayesh@rishav.me>
2025-04-15 21:23:38 +1000

    added Auto Union Deutsche Independent partner

--- src/components/partners.js
+++ src/components/partners.js
@@ -10,6 +10,7 @@ import shoyiImg from '../gallery_main/partner_photos/shoyi.webp';
 import iilumoImg from '../gallery_main/partner_photos/iilumo.webp';
 import kaImg from '../gallery_main/partner_photos/ka.webp';
 import cosmicImg from '../gallery_main/partner_photos/cosmic.webp';
+import autounionImg from '../gallery_main/partner_photos/autounion.webp';
 
 export default function Partners() {
   return (
@@ -45,7 +46,7 @@ export default function Partners() {
             name="KA Auto Sport"
             link="https://www.instagram.com/kaautosport"
             imageSrc={kaImg}
-            description="KA Auto Sport are the experts in aftermarket parts from HKS, SPOON, VOLTEX and GRUPPE-M, and wheels from RAYS, BBS and ADVAN."
+            description="KA Auto Sport are the experts in aftermarket parts from HKS, SPOON, VOLTEX and GRUPPE-M, and are authorised dealers of wheels from RAYS, BBS and ADVAN."
           />
           <PartnerCard 
             name="Cosmic Performance"
@@ -53,9 +54,14 @@ export default function Partners() {
             imageSrc={cosmicImg}
             description="Cosmic Performance is a one-stop Performance Garage that caters to all your pride and joy vehicles. Partnering with some of the best performance manufacturers and suppliers, they are able to meet the requests from supplying performance car parts, Installation and automotive engineering as well as Performance and everyday Vehicle ECU Tuning."
           />
+          <PartnerCard 
+            name="Auto Union Deutsche Independent"
+            link="https://www.instagram.com/autounion.di"
+            imageSrc={autounionImg}
+            description="With over 30 years of expertise, Auto Union specializes in European vehicles, offering comprehensive logbook servicing, maintenance and repairs, tyre services, and expert mechanical and electrical diagnostics for Audi, Volkswagen, Mercedes, BMW and much more."
+          />
         </div>
       </main>
-
       <Footer />
     </div>
   );
